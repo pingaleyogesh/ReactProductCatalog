cd92edea6c77f2d5d8d5fc7e874573a43388446d
Admin <pingaleyogesh@gmail.com>
2026-04-05 18:39:40 +0530

    Configure GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ShoppingProvider } from './context/ShoppingContext';
 import ProductPage from './components/ProductPage';
 import CartPage from './components/CartPage';
